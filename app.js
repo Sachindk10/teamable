9f8a8b4e833626d84db6fdf3bb1f4f070fdfa984
sachin <sachindk52@gmail.com>
2024-06-07 10:31:39 +0530

    Initial commit - using vanila Js

---
+++ app.js
@@ -0,0 +1,21 @@
+function handleEditProfile() {
+    document.getElementById("edit-view").style.display = "block"
+    document.getElementById("display-view").style.display = "none"
+}
+
+function handleUpdateProfile() {
+    var updateName = document.getElementById("input-name").value
+    var name = document.getElementById("name")
+    name.textContent = updateName
+
+    var updateEmail = document.getElementById("input-email").value
+    var email = document.getElementById("email")
+    email.textContent = updateEmail
+
+    var updateInterests = document.getElementById("input-interests").value
+    var interests = document.getElementById("interests")
+    interests.textContent = updateInterests
+
+    document.getElementById("edit-view").style.display = "none"
+    document.getElementById("display-view").style.display = "block"
+}
